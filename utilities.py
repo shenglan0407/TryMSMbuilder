092dbade224b466a47c926a32aec248d4c7a6dc7
shenglan <shenglan@stanford.edu>
2015-09-10 13:21:05 -0700

    convert_to_pdb function now follow the pdb format more logically

--- utilities.py
+++ utilities.py
@@ -114,8 +114,8 @@ def convert_to_pdb(filepath,savepath):
     outfile.write('%s     %s \n' % ('TITLE', 'cluster centers'))
     outfile.write('%s        %d \n' % ('MODEL' , 1))
     for ii in range(atom_num):
-        outfile.write("%6s%5.2d %4s %s %s  %s   %7.3f %7.3f %7.3f %s %5s %12s  \n" % ('HETATM', ii,'C','CLU','I',str(ii)+'D'
-        ,crds[ii,0]*10,crds[ii,1]*10,crds[ii,2]*10,'1.00','0.00','C')) 
+        outfile.write("%6s%5d %4s %3s %1s%4d%1s   %8.3f%8.3f%8.3f%6.2f%6.2f          %2s  \n" % ('HETATM', ii+1,'C','CLU','I',ii+1,'D'
+        ,crds[ii,0]*10,crds[ii,1]*10,crds[ii,2]*10, 1.0, 0.0,'C')) 
     #the factor of 10 multiplied to the coordinate is a conversion from nm to angstrom, to match the .mae file provided by DesRes
     
     outfile.write('%s \n' % 'ENDMDL')
