ff12fd2c29225007f29b7c81a535b700626c69bb
shenglan <shenglan@stanford.edu>
2015-09-09 19:07:05 -0700

    Added function to covert cluster center to pdb format

--- utilities.py
+++ utilities.py
@@ -105,3 +105,20 @@ def featurize_RawPos(indices,trajs,average=False):
         sequences.append(this_seq)
     
     return sequences
+
+def convert_to_pdb(filepath,savepath):
+    crds = np.loadtxt(filepath)
+    outfile = open(savepath,'wb')
+    atom_num = crds.shape[0]
+
+    outfile.write('%s     %s \n' % ('TITLE', 'cluster centers'))
+    outfile.write('%s        %d \n' % ('MODEL' , 1))
+    for ii in range(atom_num):
+        outfile.write("%6s%5.2d %4s %s %s  %s   %7.4f %7.4f %7.4f %s %5s %12s  \n" % ('HETATM', ii,'C','CLU','I',str(ii)+'D'
+        ,crds[ii,0]*10,crds[ii,1]*10,crds[ii,2]*10,'1.00','0.00','C')) 
+    #the factor of 10 multiplied to the coordinate is a conversion from nm to angstrom, to match the .mae file provided by DesRes
+    
+    outfile.write('%s \n' % 'ENDMDL')
+    outfile.write('%s    \n' % 'END')
+    
+    print('Done converting! \nConverted .pdb file lives in %s.' % savepath)
