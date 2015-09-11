6d789b25ef2108da1bb15d492942e9988775c97a
shenglan <shenglan@stanford.edu>
2015-09-11 16:22:31 -0700

    using visualization.py to plot frame vs cluster assignment

--- visualization.py
+++ visualization.py
@@ -1,20 +1,32 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.cm as cmap
+import pickle
 
-transmat = np.loadtxt('/home/shenglan/TryMSMbuilder/output/transmat.out')
-countsmat = np.loadtxt('/home/shenglan/TryMSMbuilder/output/countsmat.out')
+# transmat = np.loadtxt('/home/shenglan/TryMSMbuilder/output/transmat.out')
+# countsmat = np.loadtxt('/home/shenglan/TryMSMbuilder/output/countsmat.out')
+# 
+# fig1 = plt.figure()
+# plt.imshow(transmat,cmap = cmap.Blues)
+# plt.title('transition matrix')
+# plt.colorbar()
+# plt.savefig('/home/shenglan/TryMSMbuilder/output/transmat.png')
+# plt.close(fig1)
+# 
+# fig2 = plt.figure()
+# plt.imshow(countsmat,cmap = cmap.BuGn)
+# plt.title('count matrix')
+# plt.colorbar()
+# plt.savefig('/home/shenglan/TryMSMbuilder/output/countsmat.png')
+# plt.close(fig2)
 
-fig1 = plt.figure()
-plt.imshow(transmat,cmap = cmap.Blues)
-plt.title('transition matrix')
-plt.colorbar()
-plt.savefig('/home/shenglan/TryMSMbuilder/output/transmat.png')
-plt.close(fig1)
-
-fig2 = plt.figure()
-plt.imshow(countsmat,cmap = cmap.BuGn)
-plt.title('count matrix')
-plt.colorbar()
-plt.savefig('/home/shenglan/TryMSMbuilder/output/countsmat.png')
-plt.close(fig2)
+assign = pickle.load(open('/home/shenglan/TryMSMbuilder/output/KC_assign_5_sNone.out','rb'))
+fig3 = plt.figure()
+print len(assign[0])
+plt.title("one ligand's cluster assignment")
+plt.plot(assign[0],'b.',alpha = 0.5)
+plt.plot(assign[1],'g.',alpha = 0.5)
+plt.plot(assign[3],'y.',alpha = 0.5)
+plt.ylim(-0.5,4)
+plt.savefig('/home/shenglan/TryMSMbuilder/output/assignment.png')
+plt.close(fig3)
