b98b5e815f022c9877122be36828aba88527bb7e
shenglan <shenglan@stanford.edu>
2015-09-14 20:15:26 -0700

    using function from mdtraj.geometry to obtain distances btw atom pairs doulbe-checked the number I got made sense distance store in list, each item in list is ndarray n_frames by n_atompairs

--- distance_filter.py
+++ distance_filter.py
@@ -14,6 +14,8 @@ import matplotlib.pyplot as plt
 from mpl_toolkits.mplot3d import Axes3D
 import utilities as util
 
+from mdtraj.geometry import distance as md_dist
+
 from msmbuilder.cluster import KCenters
 from msmbuilder.cluster import KMedoids
 from msmbuilder.msm import MarkovStateModel
@@ -64,10 +66,29 @@ res_ind = [[atom.index for atom in target_res.atoms if atom.name in ['OD1']]]
 
 atom_pairs = []
 for this_lig in inds_N:
-    print this_lig
-    print res_ind
     for lig_atom,rec_atom in zip(this_lig,res_ind[0]):
         atom_pairs.append([lig_atom,rec_atom])
+atom_pairs = np.array(atom_pairs)
 print 'We are tracking distances between the following pairs of atoms:'     
 print atom_pairs
-    
+
+distances = []
+for this_sim in simulations:
+    this_dist = []
+    for this_traj in this_sim:
+        
+        this_dist.extend(md_dist.compute_distances(this_traj,atom_pairs))
+    this_dist = np.array(this_dist)
+    distances.append(this_dist)
+
+print len(distances)
+ 
+print distances[0].shape
+print distances[0][0]
+
+test_traj = simulations[0][0]
+a1 = np.array(test_traj.xyz[1,3,:])
+a2 = np.array(test_traj.xyz[1,1099,:])
+print np.sqrt(np.sum((a1-a2)**2))
+print md_dist.compute_distances(test_traj,[[3,1099]])
+        
