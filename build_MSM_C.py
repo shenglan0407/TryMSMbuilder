971b93651378b5c90fdfd118c02238f69e548cea
shenglan <shenglan@stanford.edu>
2015-09-16 11:14:55 -0700

    add new script to build MSM with condition C data. use load_mdtraj.py to double check that we are looking at the right atoms

---
+++ build_MSM_C.py
@@ -0,0 +1,240 @@
+#---------------------------------------------------------------------
+# Author: Shenglan Qiao <shenglan@stanford.edu>
+# Contributors:
+# Copyright (c) 2015, Stanford University
+# All rights reserved.
+
+#-----------------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------------
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d import Axes3D
+import utilities as util
+
+from msmbuilder.cluster import KCenters
+from msmbuilder.cluster import KMedoids
+from msmbuilder.msm import MarkovStateModel
+
+#-----------------------------------------------------------------------------
+# Code
+#-----------------------------------------------------------------------------
+
+#global parameters
+use_COM = False
+parent_dir = '/scratch/PI/rondror/DesRes-Simulations/B2AR_PNAS_2011a_Dror_Desres/DESRES-Trajectory_pnas2011a-C-no-water-no-lipid.1'
+number_of_sims = 10
+
+run_dirs = []
+for ii in range(number_of_sims):
+    run_dirs.append('/DESRES-Trajectory_pnas2011a-C-'+str(ii)+'-no-water-no-lipid/pnas2011a-C-'+str(ii)+'-no-water-no-lipid')
+dir_top = '/home/shenglan/topologies'
+times_path = parent_dir+run_dirs[0]+'/'+run_dirs[0].split('/')[-1]+'_times.csv'
+
+LOAD_STRIDE = 10
+
+
+#load list of mdtraj objects
+simulations = []
+for this_run_dir in run_dirs:
+    simulations.append(util.load_trajs(this_run_dir,parent_dir,dir_top,load_stride = LOAD_STRIDE))
+
+#track atoms in ligands
+inds_N =[] #indices of N atom
+atoms_to_track =['N']
+ligands = ['ALP%d'%(ii+1) for ii in range(10)]
+print ligands
+
+for ligand in ligands:
+    iis = [atom.index for atom in simulations[0][0].topology.chain(0).atoms if atom.element.symbol in atoms_to_track
+          and str(atom.residue)==ligand]
+    inds_N.append(iis)
+
+inds_all = [] #indices of all atoms, separated by ligands
+for ligand in ligands:
+    iis = [atom.index for atom in simulations[0][0].topology.chain(0).atoms if str(atom.residue) == ligand]
+    inds_all.append(iis)
+
+# #sequences of coordinates of ligands and Aps113
+total_frames = 0
+sequences_all = []
+for this_sim in simulations:
+    if use_COM:
+        this_seq = util.featurize_RawPos(inds_all,this_sim,average = True)
+    else:
+        this_seq = util.featurize_RawPos(inds_N,this_sim)
+    total_frames = this_seq[0].shape[0]*10+total_frames
+    sequences_all.extend(this_seq)
+print('the total number of conformations we are clustering is %d.' % total_frames)
+
+
+# res_pos = []
+# res_pos_ave =[]
+# total_frames = 0
+# for this_sim in simulations:
+#     this_seq = util.featurize_RawPos(res_ind,this_sim)
+#     res_pos.extend(this_seq)
+#     total_frames = total_frames + len(this_seq[0])
+#     res_pos_ave.append(np.mean(this_seq[0],axis=0))
+# 
+# print total_frames
+# print sequences_all[0][0:3]
+
+time_step = util.calc_time_step(times_path,stride = LOAD_STRIDE)
+ 
+# clustering = KCenters(n_clusters = 5000)
+# assignments = clustering.fit_predict(sequences_all)
+# centers = clustering.cluster_centers_
+# 
+# print len(assignments)
+# print assignments[1].shape
+# 
+# msm = MarkovStateModel(lag_time=300, verbose=True).fit(assignments)
+# countsmat = msm.countsmat_
+# transmat = msm.transmat_
+# #print np.sum(countsmat)
+# 
+# #np.savetxt('/home/shenglan/TryMSMbuilder/output/assignments.out',assignments, fmt = '%3.0f')
+# np.savetxt('/home/shenglan/TryMSMbuilder/output/countsmat.out',countsmat,fmt = '%8.4g')
+# np.savetxt('/home/shenglan/TryMSMbuilder/output/transmat.out',transmat,fmt = '%10.4g')
+# np.savetxt('/home/shenglan/TryMSMbuilder/output/cluster_centers.out',centers,fmt = '%10.4g')
+# np.savetxt('/home/shenglan/TryMSMbuilder/output/res_pos_ave.out',res_pos_ave,fmt = '%10.4g')
+
+#try different lag_times
+msmts0 = {}
+msmts1 = {}
+msmts2 = {}
+lag_times = [10,20,30,40,50,60,80,100,120,140]
+n_states = [500]
+
+for n in n_states:
+    msmts0[n] = []
+    msmts1[n] = []
+    msmts2[n] = []
+    for lag_time in lag_times:
+        assignments = KCenters(n_clusters=n).fit_predict(sequences_all)
+        msm = MarkovStateModel(lag_time=lag_time, verbose=False).fit(assignments)
+        timescales = msm.timescales_
+        msmts0[n].append(timescales[0])
+        msmts1[n].append(timescales[1])
+        msmts2[n].append(timescales[2])
+        print('n_states=%d\tlag_time=%.1f\ttimescales=%s (ns)' % (n, lag_time*time_step, np.array(timescales[0:3])*time_step))
+    print('-------------------')
+
+
+# #----------------------------------------------------------------------------------
+# # Visualization of data
+# #----------------------------------------------------------------------------------
+# # fig5 = plt.figure(figsize=(14,3))
+# # 
+# # plt.plot(assignments[0],'r.',alpha = 0.5)
+# # plt.plot(assignments[1],'b.',alpha = 0.5)
+# # plt.plot(assignments[2],'g.',alpha = 0.5)
+# # plt.xlabel('Frame')
+# # plt.ylabel('Cluster number')
+# # plt.title('cluster numbers, condition B')
+# # plt.savefig('/home/shenglan/TryMSMbuilder/output/fig5.png')
+# # plt.close(fig5)
+# 
+#----------------------------------------------------------------------------------
+fig4 = plt.figure(figsize=(18,5))
+plt.title('lag time vs relaxation time condition B')
+
+for i, n in enumerate(n_states):
+    plt.subplot(1,len(n_states),1+i)
+    plt.plot(np.array(lag_times)*time_step, np.array(msmts0[n])*time_step)
+    if i == 0:
+        plt.ylabel('Relaxation Timescale 1(ns)')
+    plt.xlabel('Lag Time (ns)')
+    plt.title('%d states' % n)
+
+plt.savefig('/home/shenglan/TryMSMbuilder/output/lagtime_ts1.png')
+plt.close(fig4)
+
+#----------------------------------------------------------------------------------
+fig6 = plt.figure(figsize=(18,5))
+plt.title('lag time vs relaxation time condition B')
+
+for i, n in enumerate(n_states):
+    plt.subplot(1,len(n_states),1+i)
+    plt.plot(np.array(lag_times)*time_step, np.array(msmts1[n])*time_step)
+    if i == 0:
+        plt.ylabel('Relaxation Timescale 2(ns)')
+    plt.xlabel('Lag Time (ns)')
+    plt.title('%d states' % n)
+
+plt.savefig('/home/shenglan/TryMSMbuilder/output/lagtime_ts2.png')
+plt.close(fig6)
+
+#----------------------------------------------------------------------------------
+fig7 = plt.figure(figsize=(18,5))
+plt.title('lag time vs relaxation time condition B')
+
+for i, n in enumerate(n_states):
+    plt.subplot(1,len(n_states),1+i)
+    plt.plot(np.array(lag_times)*time_step, np.array(msmts2[n])*time_step)
+    if i == 0:
+        plt.ylabel('Relaxation Timescale 3(ns)')
+    plt.xlabel('Lag Time (ns)')
+    plt.title('%d states' % n)
+
+plt.savefig('/home/shenglan/TryMSMbuilder/output/lagtime_ts3.png')
+plt.close(fig7)
+
+#----------------------------------------------------------------------------------
+# fig1 = plt.figure(figsize = (100,100))
+# 
+# for this_seq in sequences_all:
+#     for frame in this_seq:
+#         plt.scatter(frame[0],frame[1],color = 'Blue', marker = '.')
+#         plt.xlabel('x')
+#         plt.ylabel('y')
+# 
+# for point in res_pos_ave:
+#         plt.scatter(point[0],point[1],color = 'Green',marker = '*')
+# plt.savefig('/home/shenglan/TryMSMbuilder/output/fig1.png')
+# plt.close(fig1)
+
+
+#----------------------------------------------------------------------------------
+# 
+# # fig2 = plt.figure()
+# # for seq in zip(sequences_A_1,assignments):
+# #     seq = zip(seq[0],seq[1])
+# #     for point in seq:
+# #         plt.scatter(point[0][0],point[0][2],color = colors[point[1]])
+# #         plt.xlabel('x')
+# #         plt.ylabel('z')
+# # plt.scatter(res_pos_ave[0],res_pos_ave[2],color = 'Yellow', marker = '*',s= 500)
+# # plt.savefig('/home/shenglan/TryMSMbuilder/output/fig2.png')
+# # plt.close(fig2)
+# # 
+# # #----------------------------------------------------------------------------------
+# # fig3 = plt.figure()
+# # ax = fig3.add_subplot(111,projection = '3d')
+# # ax.view_init(elev=30., azim=100)
+# # 
+# # for seq in zip(sequences_A_1,assignments):
+# #     seq = zip(seq[0],seq[1])
+# #     for point in seq:
+# #         ax.scatter(point[0][0],point[0][1],point[0][2],color = colors[point[1]])
+# #         ax.set_xlabel('x')
+# #         ax.set_ylabel('y')
+# #         ax.set_zlabel('z')
+# # ax.scatter(res_pos_ave[0],res_pos_ave[1],res_pos_ave[2],color = 'Yellow', marker = '*',s=500)
+# # plt.savefig('/home/shenglan/TryMSMbuilder/output/fig3.png')
+# plt.close(fig3)
+
+# colors = np.array([x for x in 'bgrcmykbgrcmykbgrcmykbgrcmyk'])
+# colors = np.hstack([colors] * 20)
+
+# #track residue Aps113
+# first_res = str(simulations[0][0].topology.chain(0).residue(0))
+# import re
+# match = re.match(r"([a-z]+)([0-9]+)", first_res, re.I)
+# if match:
+#     items = match.groups()
+# first_res_number = int(items[-1])
+# target_res = simulations[0][0].topology.chain(0).residue(113-first_res_number)
+# res_ind = [[atom.index for atom in target_res.atoms if atom.name in ['OD1']]]
--- load_mdtraj.py
+++ load_mdtraj.py
@@ -5,13 +5,13 @@ import os
 import mdtraj as md
 
 use_COM = False
-parent_dir = '/scratch/PI/rondror/DesRes-Simulations/B2AR_PNAS_2011a_Dror_Desres/DESRES-Trajectory_pnas2011a-B-no-water-no-lipid.1'
-number_of_sims = 20
+parent_dir = '/scratch/PI/rondror/DesRes-Simulations/B2AR_PNAS_2011a_Dror_Desres/DESRES-Trajectory_pnas2011a-C-no-water-no-lipid.1'
+number_of_sims = 1
 
 
 run_dirs = []
 for ii in range(number_of_sims):
-    run_dirs.append('/DESRES-Trajectory_pnas2011a-B-'+str(ii)+'-no-water-no-lipid/pnas2011a-B-'+str(ii)+'-no-water-no-lipid')
+    run_dirs.append('/DESRES-Trajectory_pnas2011a-C-'+str(ii)+'-no-water-no-lipid/pnas2011a-C-'+str(ii)+'-no-water-no-lipid')
 dir_top = '/home/shenglan/topologies'
 times_path = parent_dir+run_dirs[0]+'/'+run_dirs[0].split('/')[-1]+'_times.csv'
 
@@ -29,13 +29,17 @@ test_traj = util.load_trajs(test_run,parent_dir,dir_top,load_stride = LOAD_STRID
 
 inds_N =[] #indices of N atom
 atoms_to_track =['N']
-ligands = [residue for residue in test_traj[0].topology.chain(0).residues][0:10]
+ligands = ['ALP%d'%(ii+1) for ii in range(10)]
+print ligands
 
+print test_traj[0].topology.chain(0).atom(4667).residue
 for ligand in ligands:
     iis = [atom.index for atom in test_traj[0].topology.chain(0).atoms if atom.element.symbol in atoms_to_track
-          and atom.residue == ligand]
+          and str(atom.residue)==ligand]
     inds_N.append(iis)
 
+# inds_N = [[atom.index] for atom in test_traj[0].topology.chain(0).atoms if atom.element.symbol in atoms_to_track
+#            and atom.residue.name== 'ALP']
 print inds_N
 
 test_seq = util.featurize_RawPos(inds_N,test_traj)
@@ -45,7 +49,7 @@ print test_seq[1][0]
 for atom_idx in inds_N: 
     print(tuple(test_traj[0].xyz[0, atom_idx,:]))
 
-print(tuple(test_traj[0].xyz[0, 91,:]))
+print(tuple(test_traj[0].xyz[0, 4979,:]))
 
 print test_traj[0]
 # 
