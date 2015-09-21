e249401efed073ae929ffcc292cda6fac4b9218d
shenglan <shenglan@stanford.edu>
2015-09-21 11:14:11 -0700

    create script to build and lump ergodic transition matrices for data in condition C

---
+++ build_ergodic_MSM_C.py
@@ -0,0 +1,254 @@
+#---------------------------------------------------------------------
+# Author: Shenglan Qiao <shenglan@stanford.edu>
+# Contributors:
+# Copyright (c) 2015, Stanford University
+# All rights reserved.
+
+#-----------------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------------
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+from mpl_toolkits.mplot3d import Axes3D
+import utilities as util
+
+from msmbuilder.cluster import KCenters
+from msmbuilder.cluster import KMedoids
+from msmbuilder.msm import MarkovStateModel
+#-----------------------------------------------------------------------------
+# matplotlib settings
+#-----------------------------------------------------------------------------
+mpl.rcParams['font.size'] = 24.0
+#-----------------------------------------------------------------------------
+# Code
+#-----------------------------------------------------------------------------
+
+#global parameters
+use_COM = False
+parent_dir = '/scratch/PI/rondror/DesRes-Simulations/B2AR_PNAS_2011a_Dror_Desres/DESRES-Trajectory_pnas2011a-C-no-water-no-lipid.1'
+number_of_sims = 10
+
+run_dirs = []
+for ii in range(number_of_sims):
+    run_dirs.append('/DESRES-Trajectory_pnas2011a-C-'+str(ii)+'-no-water-no-lipid/pnas2011a-C-'+str(ii)+'-no-water-no-lipid')
+dir_top = '/home/shenglan/topologies'
+times_path = parent_dir+run_dirs[0]+'/'+run_dirs[0].split('/')[-1]+'_times.csv'
+
+LOAD_STRIDE = None
+N_CLUSTER = 1000
+
+#load list of mdtraj objects
+simulations = []
+for this_run_dir in run_dirs:
+    simulations.append(util.load_trajs(this_run_dir,parent_dir,dir_top,load_stride = LOAD_STRIDE))
+
+#track atoms in ligands
+inds_N =[] #indices of N atom
+atoms_to_track =['N']
+ligands = ['ALP%d'%(ii+1) for ii in range(10)]
+
+
+for ligand in ligands:
+    iis = [atom.index for atom in simulations[0][0].topology.chain(0).atoms if atom.element.symbol in atoms_to_track
+          and str(atom.residue)==ligand]
+    inds_N.append(iis)
+
+inds_all = [] #indices of all atoms, separated by ligands
+for ligand in ligands:
+    iis = [atom.index for atom in simulations[0][0].topology.chain(0).atoms if str(atom.residue) == ligand]
+    inds_all.append(iis)
+
+# #sequences of coordinates of ligands and Aps113
+total_frames = 0
+sequences_all = []
+for this_sim in simulations:
+    if use_COM:
+        this_seq = util.featurize_RawPos(inds_all,this_sim,average = True)
+    else:
+        this_seq = util.featurize_RawPos(inds_N,this_sim)
+    total_frames = this_seq[0].shape[0]*10+total_frames
+    sequences_all.extend(this_seq)
+print('the total number of conformations we are clustering is %d.' % total_frames)
+
+
+
+time_step = util.calc_time_step(times_path,stride = LOAD_STRIDE)
+ 
+clustering = KCenters(n_clusters = N_CLUSTER)
+assignments = clustering.fit_predict(sequences_all)
+centers = clustering.cluster_centers_
+# 
+# print len(assignments)
+# print assignments[1].shape
+# 
+msm = MarkovStateModel(lag_time=4000, sliding_window = True, verbose=True).fit(assignments)
+countsmat = msm.countsmat_
+transmat = msm.transmat_
+print countsmat.shape
+
+#try different lag_times
+msmts0 = {}
+msmts1 = {}
+msmts2 = {}
+lag_times = [100,500,1000,1500,2000,2500,3000,3500,4000]
+n_states = [N_CLUSTER]
+
+for n in n_states:
+    msmts0[n] = []
+    msmts1[n] = []
+    msmts2[n] = []
+
+    for lag_time in lag_times:
+        this_msm = MarkovStateModel(lag_time=lag_time,sliding_window = True, verbose=False).fit(assignments)
+        timescales = this_msm.timescales_
+        msmts0[n].append(timescales[0])
+        msmts1[n].append(timescales[1])
+        msmts2[n].append(timescales[2])
+        print('n_states=%d\tlag_time=%.1f\ttimescales=%s (ns)' % (n, lag_time*time_step, np.array(timescales[0:3])*time_step))
+    print('-------------------')
+
+
+#----------------------------------------------------------------------------------
+# Save data
+#----------------------------------------------------------------------------------
+msm_path = '/home/shenglan/TryMSMbuilder/output/C/ergodic/KC_msm_c'+str(N_CLUSTER)+'_s'+str(LOAD_STRIDE)+'.out'
+pickle.dump(msm,open(msm_path,'wb'))
+
+seq_path = '/home/shenglan/TryMSMbuilder/output/C/ergodic/sequences'+'_s'+str(LOAD_STRIDE)+'.out'
+pickle.dump(sequences_all,open(seq_path,'wb'))
+
+assign_path = '/home/shenglan/TryMSMbuilder/output/C/ergodic/KC_assign_c'+str(N_CLUSTER)+'_s'+str(LOAD_STRIDE)+'.out'
+pickle.dump(assignments,open(assign_path,'wb'))
+
+countsmat_path = '/home/shenglan/TryMSMbuilder/output/C/ergodic/KC_countsmat_c'+str(N_CLUSTER)+'_s'+str(LOAD_STRIDE)+'.out'
+np.savetxt(countsmat_path,countsmat,fmt = '%8.4g')
+
+transmat_path = '/home/shenglan/TryMSMbuilder/output/C/ergodic/KC_transmat_c'+str(N_CLUSTER)+'_s'+str(LOAD_STRIDE)+'.out'
+np.savetxt(transmat_path,transmat,fmt = '%10.4g')
+
+centers_path = '/home/shenglan/TryMSMbuilder/output/C/ergodic/KC_centers_c'+str(N_CLUSTER)+'_s'+str(LOAD_STRIDE)+'.out'
+np.savetxt(centers_path,centers,fmt = '%10.4g')
+
+#----------------------------------------------------------------------------------
+# Convert to pdb
+#----------------------------------------------------------------------------------
+pdb_path = '/home/shenglan/TryMSMbuilder/output/C/ergodic/KC_c'+str(N_CLUSTER)+'_s'+str(LOAD_STRIDE)+'.pdb'
+util.convert_sequences_to_pdb(seq_path,assign_path,pdb_path)
+
+#----------------------------------------------------------------------------------
+# Visualization of data
+#----------------------------------------------------------------------------------
+# fig5 = plt.figure(figsize=(14,3))
+# 
+# plt.plot(assignments[0],'r.',alpha = 0.5)
+# plt.plot(assignments[1],'b.',alpha = 0.5)
+# plt.plot(assignments[2],'g.',alpha = 0.5)
+# plt.xlabel('Frame')
+# plt.ylabel('Cluster number')
+# plt.title('cluster numbers, condition B')
+# plt.savefig('/home/shenglan/TryMSMbuilder/output/fig5.png')
+# plt.close(fig5)
+# 
+#----------------------------------------------------------------------------------
+fig4 = plt.figure(figsize=(18,8))
+plt.title('lag time vs relaxation time condition B')
+
+for i, n in enumerate(n_states):
+    plt.subplot(1,len(n_states),1+i)
+    plt.plot(np.array(lag_times)*time_step, np.array(msmts0[n])*time_step)
+    if i == 0:
+        plt.ylabel('Relaxation Timescale 1(ns)')
+    plt.xlabel('Lag Time (ns)')
+    plt.title('%d states' % n)
+
+plt.savefig('/home/shenglan/TryMSMbuilder/output/C/ergodic/lagtime_ts1.png')
+plt.close(fig4)
+
+#----------------------------------------------------------------------------------
+fig6 = plt.figure(figsize=(18,8))
+plt.title('lag time vs relaxation time condition B')
+
+for i, n in enumerate(n_states):
+    plt.subplot(1,len(n_states),1+i)
+    plt.plot(np.array(lag_times)*time_step, np.array(msmts1[n])*time_step)
+    if i == 0:
+        plt.ylabel('Relaxation Timescale 2(ns)')
+    plt.xlabel('Lag Time (ns)')
+    plt.title('%d states' % n)
+
+plt.savefig('/home/shenglan/TryMSMbuilder/output/C/ergodic/lagtime_ts2.png')
+plt.close(fig6)
+
+#----------------------------------------------------------------------------------
+fig7 = plt.figure(figsize=(18,8))
+plt.title('lag time vs relaxation time condition B')
+
+for i, n in enumerate(n_states):
+    plt.subplot(1,len(n_states),1+i)
+    plt.plot(np.array(lag_times)*time_step, np.array(msmts2[n])*time_step)
+    if i == 0:
+        plt.ylabel('Relaxation Timescale 3(ns)')
+    plt.xlabel('Lag Time (ns)')
+    plt.title('%d states' % n)
+
+plt.savefig('/home/shenglan/TryMSMbuilder/output/C/ergodic/lagtime_ts3.png')
+plt.close(fig7)
+
+#----------------------------------------------------------------------------------
+# fig1 = plt.figure(figsize = (100,100))
+# 
+# for this_seq in sequences_all:
+#     for frame in this_seq:
+#         plt.scatter(frame[0],frame[1],color = 'Blue', marker = '.')
+#         plt.xlabel('x')
+#         plt.ylabel('y')
+# 
+# for point in res_pos_ave:
+#         plt.scatter(point[0],point[1],color = 'Green',marker = '*')
+# plt.savefig('/home/shenglan/TryMSMbuilder/output/fig1.png')
+# plt.close(fig1)
+
+
+#----------------------------------------------------------------------------------
+# 
+# # fig2 = plt.figure()
+# # for seq in zip(sequences_A_1,assignments):
+# #     seq = zip(seq[0],seq[1])
+# #     for point in seq:
+# #         plt.scatter(point[0][0],point[0][2],color = colors[point[1]])
+# #         plt.xlabel('x')
+# #         plt.ylabel('z')
+# # plt.scatter(res_pos_ave[0],res_pos_ave[2],color = 'Yellow', marker = '*',s= 500)
+# # plt.savefig('/home/shenglan/TryMSMbuilder/output/fig2.png')
+# # plt.close(fig2)
+# # 
+# # #----------------------------------------------------------------------------------
+# # fig3 = plt.figure()
+# # ax = fig3.add_subplot(111,projection = '3d')
+# # ax.view_init(elev=30., azim=100)
+# # 
+# # for seq in zip(sequences_A_1,assignments):
+# #     seq = zip(seq[0],seq[1])
+# #     for point in seq:
+# #         ax.scatter(point[0][0],point[0][1],point[0][2],color = colors[point[1]])
+# #         ax.set_xlabel('x')
+# #         ax.set_ylabel('y')
+# #         ax.set_zlabel('z')
+# # ax.scatter(res_pos_ave[0],res_pos_ave[1],res_pos_ave[2],color = 'Yellow', marker = '*',s=500)
+# # plt.savefig('/home/shenglan/TryMSMbuilder/output/fig3.png')
+# plt.close(fig3)
+
+# colors = np.array([x for x in 'bgrcmykbgrcmykbgrcmykbgrcmyk'])
+# colors = np.hstack([colors] * 20)
+
+# #track residue Aps113
+# first_res = str(simulations[0][0].topology.chain(0).residue(0))
+# import re
+# match = re.match(r"([a-z]+)([0-9]+)", first_res, re.I)
+# if match:
+#     items = match.groups()
+# first_res_number = int(items[-1])
+# target_res = simulations[0][0].topology.chain(0).residue(113-first_res_number)
+# res_ind = [[atom.index for atom in target_res.atoms if atom.name in ['OD1']]]
---
+++ lumping_ergodic_C.py
@@ -0,0 +1,126 @@
+#---------------------------------------------------------------------
+# Author: Shenglan Qiao <shenglan@stanford.edu>
+# Contributors:
+# Copyright (c) 2015, Stanford University
+# All rights reserved.
+
+#-----------------------------------------------------------------------------
+# Imports
+#-----------------------------------------------------------------------------
+import numpy as np
+import pickle
+import matplotlib.pyplot as plt
+import copy
+
+import utilities as util
+
+from msmbuilder.lumping import PCCA, PCCAPlus
+from msmbuilder.msm import MarkovStateModel
+
+#-----------------------------------------------------------------------------
+# Code
+#-----------------------------------------------------------------------------
+LOAD_STRIDE = None
+model = 'c1000_s'+str(LOAD_STRIDE)
+micro_msm_path = '/home/shenglan/TryMSMbuilder/output/C/ergodic/KC_msm_'+model+'.out'
+micro_msm = pickle.load(open(micro_msm_path,'rb'))
+
+geo_assign_path = '/home/shenglan/TryMSMbuilder/output/C/ergodic/KC_assign_'+model+'.out'
+geo_assign = pickle.load(open(geo_assign_path,'rb'))
+#print geo_assign.shape
+print('There are %d microstates in msm' % micro_msm.n_states_)
+
+raw_clusters = []
+for this_assign in geo_assign:
+    raw_clusters.extend(np.unique(this_assign))
+raw_clusters = np.unique(np.array(raw_clusters))
+print('There are %d clusters in the original geometric clustering.'%len(raw_clusters))
+
+
+raw_to_micro_mapping = copy.copy(micro_msm.mapping_)
+N_MICRO = len(raw_to_micro_mapping)
+dummy_assign = N_MICRO
+for ii in range(len(raw_clusters)):
+    if ii in raw_to_micro_mapping.keys():
+        pass
+    else:
+        raw_to_micro_mapping[ii] = dummy_assign
+        dummy_assign = dummy_assign+1
+        
+micro_assign = []
+
+for this_assign in geo_assign:
+    this_list = np.zeros(len(this_assign),dtype = int)
+    micro_assign.append(this_list)
+    
+    
+for nn in range(len(micro_assign)):
+    for ii in range(len(micro_assign[nn])):
+        micro_assign[nn][ii] = raw_to_micro_mapping[geo_assign[nn][ii]]
+
+#check for mismatch
+for this_assign, that_assign in zip(micro_assign,geo_assign):
+    for this_item,that_item in zip(this_assign,that_assign):
+        if this_item == raw_to_micro_mapping[that_item]:
+            pass
+        else:
+            print ('mismatch %d, %d' % (this_item, that_item))
+
+partial_raw_to_micro_mapping = copy.copy(micro_msm.mapping_)
+partial_micro_assign = []
+for this_assign in geo_assign:
+    this_list = []
+    for this_item in this_assign:
+        try:
+            this_list.append(partial_raw_to_micro_mapping[this_item])
+        except KeyError:
+            pass
+    partial_micro_assign.append(this_list)
+    
+unique_assign = []
+for this_assign in partial_micro_assign:
+    unique_assign.extend(np.unique(this_assign))
+unique_assign = np.unique(np.array(unique_assign))
+
+N_MACRO = 10
+pcca = PCCA.from_msm(micro_msm,N_MACRO)
+micro_to_macro_mapping = {}
+for ii in range(len(pcca.microstate_mapping_)):
+    micro_to_macro_mapping[ii] = pcca.microstate_mapping_[ii]
+
+# for n macrostates, any frame that does not belong to any microstate and thus macrostate is labeled to be in the nth macrostate
+for ii in range(len(raw_clusters)):
+    if ii in micro_to_macro_mapping.keys():
+        pass
+    else:
+        micro_to_macro_mapping[ii] = N_MACRO
+    
+# assignments to macro states
+macro_assign = []
+
+for this_assign in geo_assign:
+    this_list = np.zeros(len(this_assign),dtype = int)
+    macro_assign.append(this_list)
+
+for nn in range(len(macro_assign)):
+    for ii in range(len(macro_assign[nn])):
+        macro_assign[nn][ii] = micro_to_macro_mapping[micro_assign[nn][ii]]
+
+macro_assign_path = '/home/shenglan/TryMSMbuilder/output/C/ergodic/KC_macroassign_c'+str(N_MACRO)+'_s'+str(LOAD_STRIDE)+'.out'
+pickle.dump(macro_assign,open(macro_assign_path,'wb'))
+
+micro_assign_path = '/home/shenglan/TryMSMbuilder/output/C/ergodic/KC_microassign_c'+str(N_MICRO)+'_s'+str(LOAD_STRIDE)+'.out'
+pickle.dump(micro_assign,open(micro_assign_path,'wb'))
+
+seq_path = '/home/shenglan/TryMSMbuilder/output/C/ergodic/sequences_s'+str(LOAD_STRIDE)+'.out'
+
+save_macro_pdb_path = '/home/shenglan/TryMSMbuilder/output/C/ergodic/KC_macroassign_c'+str(N_MACRO)+'_s'+str(LOAD_STRIDE)+'.pdb'
+save_micro_pdb_path = '/home/shenglan/TryMSMbuilder/output/C/ergodic/KC_microassign_c'+str(N_MICRO)+'_s'+str(LOAD_STRIDE)+'.pdb'
+
+util.convert_sequences_to_pdb(seq_path,macro_assign_path,save_macro_pdb_path)
+util.convert_sequences_to_pdb(seq_path,micro_assign_path,save_micro_pdb_path)
+
+
+# macro_assign = pcca.fit(partial_micro_assign)
+# 
+# print macro_assign
