8fdd9f422826cbb2b5140dcf32b896bde3b2d5eb
shenglan <shenglan@stanford.edu>
2015-09-21 17:36:36 -0700

    use this script to investigate countsmat

--- eigensys.py
+++ eigensys.py
@@ -2,19 +2,36 @@ import pickle
 import numpy as np
 import matplotlib.pyplot as plt
 
-model = 'c2000_s10.out'
-micro_msm_path = '/home/shenglan/TryMSMbuilder/output/C/KC_msm_'+model
+model = 'c1000_sNone.out'
+micro_msm_path = '/home/shenglan/TryMSMbuilder/output/C/all_clusters/KC_msm_'+model
 micro_msm = pickle.load(open(micro_msm_path,'rb'))
 
-timescales = micro_msm.timescales_
-# print timescales.shape
-# print timescales[1290:]
+countsmat = micro_msm.countsmat_
+# for ii in range(countsmat.shape[0]):
+#     if np.sum(countsmat[ii][:]) == 0.0:
+#         print ii
+#         print np.sum(countsmat[ii][:])
+rev_counts = (countsmat+countsmat.T)*0.5
 
-eigenvalues = micro_msm.eigenvalues_
-right_eigenvectors = micro_msm.right_eigenvectors_
+for ii in range(rev_counts.shape[0]):
+    if np.sum(rev_counts[ii][:]) == 0.0:
+        print ii
+        print np.sum(rev_counts[ii][:])
 
-test_vector = right_eigenvectors[:,-1]
-print test_vector[0]
+transmat = micro_msm.transmat_
+
+print countsmat[(countsmat < 1.0) & (countsmat > 0.0)]
+if True in np.isnan(transmat):
+    print('there is nan')
+# timescales = micro_msm.timescales_
+# # print timescales.shape
+# # print timescales[1290:]
+# 
+# eigenvalues = micro_msm.eigenvalues_
+# right_eigenvectors = micro_msm.right_eigenvectors_
+# 
+# test_vector = right_eigenvectors[:,-1]
+# print test_vector[0]
 # print right_eigenvectors.shape
 # print eigenvalues[1290:]
 # fig1 = plt.figure()
