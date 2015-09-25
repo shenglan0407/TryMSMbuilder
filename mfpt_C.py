f3de6c3dae76ddd12e97b5d60531799f3db31ed8
shenglan <shenglan@stanford.edu>
2015-09-24 21:33:21 -0700

    calculating mfpt_on and mfpt_off now for estimating kinetic rates

--- mfpt_C.py
+++ mfpt_C.py
@@ -8,7 +8,7 @@ import copy
 
 LOAD_STRIDE = 10
 N_MICRO = 1300
-BULK_CUTOFF = 2.0 #unit is nm
+BULK_CUTOFF = 6.75 #unit is nm
 
 model = 'c'+str(N_MICRO)+'_s'+str(LOAD_STRIDE)
 micro_msm_path = '/home/shenglan/TryMSMbuilder/output/C/all_clusters/KC_msm_'+model+'.out'
@@ -38,14 +38,32 @@ for ii in range(len(distances)):
         print len(distances[ii]) 
         print len(micro_assign[ii])
 
-# Identify 'sinks' for mfpt calculations
+# Identify 'sinks' for mfpt_off calculations
 bulk_clusters = []
+bound_clusters = [288,1020]
 for ii in clusters_dist.keys():
     if np.average(clusters_dist[ii]) > BULK_CUTOFF:
         bulk_clusters.append(ii)
+   #  else:
+#         bound_clusters.append(ii)
 print len(bulk_clusters)
     
-mfpt_mat = mfpt.mfpts(micro_msm,sinks = bulk_clusters,lag_time = 80*1.8)
-print mfpt_mat.shape
-print mfpt_mat[mfpt_mat > 1000.0]
-print np.where(mfpt_mat> 1000.0)
+mfpt_off = mfpt.mfpts(micro_msm,sinks = bulk_clusters,lag_time = 80*1.8)
+mfpt_on = mfpt.mfpts(micro_msm,sinks = bound_clusters,lag_time = 80*1.8)
+#print mfpt_mat.shape
+print "Here are so mfpt_off times (ns) and corresponding cluster ids:"
+print mfpt_off[mfpt_off > 7.354e6]
+print np.where(mfpt_off> 7.354e6)
+print ("Maximum mfpt_off time is %.3g ns in cluster %d." % (np.max(mfpt_off),np.argmax(mfpt_off)))
+print ("and the mean mfpt_off is %.3g ns"%np.mean(mfpt_off))
+
+print ("Maximum mfpt_on time is %.g ns in cluster %d." % (np.max(mfpt_on),np.argmax(mfpt_on)))
+print ("and the mean mfpt_on is %.3g ns"%np.mean(mfpt_on))
+
+mfpt_on_path = '/home/shenglan/TryMSMbuilder/output/C/all_clusters/KC_mfpton_c'+str(N_MICRO)+'_s'+str(LOAD_STRIDE)+'.out'
+mfpt_off_path = '/home/shenglan/TryMSMbuilder/output/C/all_clusters/KC_mfptoff_c'+str(N_MICRO)+'_s'+str(LOAD_STRIDE)+'.out'
+
+pickle.dump(mfpt_on,open(mfpt_on_path,'wb'))
+pickle.dump(mfpt_off,open(mfpt_off_path,'wb'))
+
+volume = ()
